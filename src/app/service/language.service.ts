90caa78d1dc2fc6af1d6a709ba18ed8a7c258cbb
Parteek Sharma <sparteek91@gmail.com>
2023-09-12 14:03:08 +0530

    EN/HINDI-Through API

--- src/app/service/language.service.ts
+++ src/app/service/language.service.ts
@@ -1,23 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs/internal/Observable';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
-  private readonly url =
-    'https://translation.googleapis.com/language/translate/v2?key=YOUR_GOOGLE_API_KEY';
+  private readonly apiKey = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
+  private readonly apiUrl = 'https://translation.googleapis.com/language/translate/v2';
 
-  constructor(private _http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   translate(text: string, targetLanguage: string): Observable<any> {
-    const requestBody = {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const params = {
       q: text,
       target: targetLanguage,
+      format: 'text',
+      key: this.apiKey,
     };
 
-    return this._http.post(this.url, requestBody);
+    return this.http.post(this.apiUrl, params, { headers });
   }
+
 }
